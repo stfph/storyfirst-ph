c5e1e6606c7130ebafce547cafb5dcc5bbee19b4
Fence <johnrey.aparecio@gmail.com>
2026-09-01 20:52:45 +0800

    fix(deploy): correct vite base path to resolve CORS asset errors

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/hfqhi/storyfirst-ph",
+  base: "/https://github.com/hfqhi/storyfirst-ph/",
 });
