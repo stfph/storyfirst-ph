b95915a087cd64d3b98e53f159fcf7b3a917ba92
Fence <johnrey.aparecio@gmail.com>
2026-09-01 20:29:54 +0800

    ci(deploy): configure vite base path and setup github actions workflow for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/storyfirst-ph/", // for github pages deployment
+  plugins: [react()],
+  base: "https://github.com/hfqhi/storyfirst-ph",
 });
